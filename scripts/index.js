c2cc6512eb66fb157d02db8da1eeb1fd88e89bd8
premj44 <premkumarjadhav9044@gmail.com>
2023-02-25 13:55:23 +0530

    project

---
+++ scripts/index.js
@@ -0,0 +1,120 @@
+let arr=
+[
+    "https://www.jcrew.com/brand_creative/2022/202205-May/cc_newarrivals/2022may_0426_na_g_img0.jpg",
+    "https://www.jcrew.com/brand_creative/2022/202205-May/cc_newarrivals/2022may_0426_na_g_img6.jpg",
+    "https://www.jcrew.com/brand_creative/2022/202205-May/cc_newarrivals/2022may_0426_na_b_img0.jpg",
+    "https://www.jcrew.com/brand_creative/2022/202205-May/cc_newarrivals/2022may_0426_na_b_img7.jpg"
+    
+]
+let i=0;
+let x=document.getElementById("land8")
+let id=setInterval(() => {
+    if(i==arr.length){
+        i=0;
+    }
+    x.style.backgroundImage=`url(${arr[i]})`
+    i++
+}, 1000);
+
+
+
+let show1=()=>{
+    document.getElementById("spr61").style.display="block"
+}
+
+let hide1=()=>{
+    document.getElementById("spr61").style.display="none"
+}
+
+let show2=()=>{
+    document.getElementById("spr62").style.display="block"
+}
+
+let hide2=()=>{
+    document.getElementById("spr62").style.display="none"
+}
+
+let show3=()=>{
+    document.getElementById("spr63").style.display="block"
+}
+
+let hide3=()=>{
+    document.getElementById("spr63").style.display="none"
+}
+
+let show4=()=>{
+    document.getElementById("spr64").style.display="block"
+}
+
+let hide4=()=>{
+    document.getElementById("spr64").style.display="none"
+}
+
+
+document.getElementById("pr6img1").addEventListener("mouseover",show1)
+document.getElementById("pr6img1").addEventListener("mouseleave",hide1)
+
+document.getElementById("pr6img2").addEventListener("mouseover",show2)
+document.getElementById("pr6img2").addEventListener("mouseleave",hide2)
+
+document.getElementById("pr6img3").addEventListener("mouseover",show3)
+document.getElementById("pr6img3").addEventListener("mouseleave",hide3)
+
+document.getElementById("pr6img4").addEventListener("mouseover",show4)
+document.getElementById("pr6img4").addEventListener("mouseleave",hide4)
+
+
+
+let gotowomen=()=>{
+    window.location.href="womenpage.html"
+}
+
+let gotomen=()=>{
+    window.location.href="menpage.html"
+}
+
+let gotokid=()=>{
+    window.location.href="kidspage.html"
+}
+
+
+document.getElementById("pr41").addEventListener("click",gotowomen);
+document.getElementById("pr42").addEventListener("click",gotomen);
+document.getElementById("pr43").addEventListener("click",gotokid);
+document.getElementById("pr44").addEventListener("click",gotokid);
+
+
+
+//************************ //
+
+let goright=()=>{
+    document.getElementById("pr6img1").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/w_swim/2022may_0426_w_swim_img2.jpg')"
+    document.getElementById("like1").innerText="579 Likes"
+
+    document.getElementById("pr6img2").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0506_flyouts_m_img0.jpg')"
+    document.getElementById("like2").innerText="784 Likes"
+
+    document.getElementById("pr6img3").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0426_flyouts_cc_img2.jpg')"
+    document.getElementById("like3").innerText="1718 Likes"
+
+    document.getElementById("pr6img4").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0506_flyouts_stories_img0.jpg')"
+    document.getElementById("like4").innerText="987 Likes"
+}
+document.getElementById("sign2").addEventListener("click", goright)
+
+
+let goleft=()=>{
+    document.getElementById("pr6img1").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BA400_OR5362_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like1").innerText="416 Likes"
+
+    document.getElementById("pr6img2").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BI759_OR5362_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like2").innerText="793 Likes"
+
+    document.getElementById("pr6img3").style.backgroundImage="url('https://www.jcrew.com/s7-img-facade/BI409_YD1226_m?fmt=jpeg&qlt=90,0&resMode=sharp&op_usm=.1,0,0,0&crop=0,0,0,0&wid=540&hei=540')"
+    document.getElementById("like3").innerText="1412 Likes"
+
+    document.getElementById("pr6img4").style.backgroundImage="url('https://www.jcrew.com/brand_creative/2022/202205-May/flyout/2022may_0426_flyouts_cc_img0.jpg')"
+    document.getElementById("like4").innerText="1629 Likes"
+}
+document.getElementById("sign1").addEventListener("click", goleft)
+
---
+++ scripts/nav.js
@@ -0,0 +1,352 @@
+import {navbar} from "../components/navbar.js"
+document.getElementById("body").innerHTML=navbar()
+
+// let q="Sign In"
+// localStorage.setItem("sign",JSON.stringify("Sign In"))
+
+
+let dltfromstorage=()=>{
+    let dl=JSON.parse(localStorage.getItem("sign"));
+    dl=null;
+    localStorage.setItem("sign",JSON.stringify(dl))
+    localStorage.removeItem("cart")
+    // let dltc=JSON.parse(localStorage.getItem("cart"));
+    // dltc=[];
+    // localStorage.setItem("cart",JSON.stringify(dltc))
+    // localStorage.clear()
+
+
+    window.location.href="index.html"
+}
+
+document.getElementById("dlt").addEventListener("click",dltfromstorage)
+
+
+
+
+let query=JSON.parse(localStorage.getItem("sign"))
+console.log(query)
+if(query=="My Account"){
+    document.getElementById("navacc").innerHTML="My Account";
+}else{
+    document.getElementById("navacc").innerHTML="Sign In"
+}
+
+
+document.getElementById("navbarsearch").addEventListener("keypress", function(event){
+    let x=document.getElementById("navbarsearch").value
+    if(event.key==="Enter"){
+    
+        if(x=="Mens"||x=="mens"||x=="Men"||x=="men"){
+            window.location.href="menpage.html"
+        }else if(x=="Women"||x=="women"||x=="Womens"||x=="womens"){
+            window.location.href="womenpage.html"
+        }else if(x=="kid"||x=="kids"||x=="Kid"||x=="Kids"){
+            window.location.href="kidspage.html"
+        }
+    }
+
+})
+
+let gotoindex=()=>{
+    window.location.href="index.html"
+
+}
+document.getElementById("navimg").addEventListener("click",gotoindex);
+
+
+let myNew=()=>{
+    let x=document.getElementById("new")
+    // x.style.display="block"
+    x.style.display="flex"
+
+    
+
+    
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    
+    
+    
+    
+
+}
+let hideNew=()=>{
+   
+    let x=document.getElementById("new")
+    x.style.display="none"
+    
+}
+
+
+
+
+let mywomen=()=>{
+    
+    document.getElementById("men").style.display="none"
+    document.getElementById("new").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("women")
+    // x.style.display="block"
+    x.style.display="flex"
+
+}
+let hidewomen=()=>{
+    let x=document.getElementById("women")
+    x.style.display="none"
+}
+
+
+
+
+
+let mymen=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("men")
+    // x.style.display="block"
+    x.style.display="flex"
+
+}
+let hidemen=()=>{
+
+    let x=document.getElementById("men")
+    x.style.display="none"
+}
+
+let mykids=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("kids")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hidekids=()=>{
+    let x=document.getElementById("kids")
+    x.style.display="none"
+}
+
+let myswim=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("swim")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hideswim=()=>{
+    let x=document.getElementById("swim")
+    x.style.display="none"
+}
+
+let mycashmere=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("cashmere")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hidecashmere=()=>{
+    let x=document.getElementById("cashmere")
+    x.style.display="none"
+}
+
+
+let myhome=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+
+    let x=document.getElementById("home")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hidehome=()=>{
+    let x=document.getElementById("home")
+    x.style.display="none"
+}
+
+
+
+let mystories=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("stories")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hidestories=()=>{
+    let x=document.getElementById("stories")
+    x.style.display="none"
+}
+
+let mysale=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("acc").style.display="none"
+    let x=document.getElementById("sale")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hidesale=()=>{
+    let x=document.getElementById("sale")
+    x.style.display="none"
+}
+
+let myacc=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    let x=document.getElementById("acc")
+    // x.style.display="block"
+    x.style.display="flex"
+}
+
+let hideacc=()=>{
+    let x=document.getElementById("acc")
+    x.style.display="none"
+}
+
+
+
+
+
+
+let hidediv=()=>{
+    document.getElementById("new").style.display="none"
+    document.getElementById("women").style.display="none"
+    document.getElementById("men").style.display="none"
+    document.getElementById("kids").style.display="none"
+    document.getElementById("swim").style.display="none"
+    document.getElementById("cashmere").style.display="none"
+    document.getElementById("home").style.display="none"
+    document.getElementById("stories").style.display="none"
+    document.getElementById("sale").style.display="none"
+    document.getElementById("acc").style.display="none"
+}
+
+
+document.getElementById("navnew").addEventListener("mouseover",myNew);
+document.getElementById("new").addEventListener("mouseleave",hideNew);
+
+document.getElementById("navwomen").addEventListener("mouseover",mywomen);
+document.getElementById("women").addEventListener("mouseleave",hidewomen);
+
+document.getElementById("navmen").addEventListener("mouseover",mymen);
+document.getElementById("men").addEventListener("mouseleave",hidemen);
+
+
+document.getElementById("navkids").addEventListener("mouseover",mykids);
+document.getElementById("kids").addEventListener("mouseleave",hidekids);
+
+document.getElementById("navswim").addEventListener("mouseover",myswim);
+document.getElementById("swim").addEventListener("mouseleave",hideswim);
+
+document.getElementById("navcashmere").addEventListener("mouseover",mycashmere);
+document.getElementById("cashmere").addEventListener("mouseleave",hidecashmere);
+
+
+document.getElementById("navhome").addEventListener("mouseover",myhome);
+document.getElementById("home").addEventListener("mouseleave",hidehome);
+
+
+document.getElementById("navstories").addEventListener("mouseover",mystories);
+document.getElementById("stories").addEventListener("mouseleave",hidestories);
+
+document.getElementById("navsale").addEventListener("mouseover",mysale);
+document.getElementById("sale").addEventListener("mouseleave",hidesale);
+
+let it=document.getElementById("navacc").innerText
+if(it=="My Account"){
+    document.getElementById("navacc").addEventListener("mouseover",myacc);
+    document.getElementById("acc").addEventListener("mouseleave",hideacc);
+}
+
+
+
+document.getElementById("navimg").addEventListener("mouseover",hidediv);
+document.getElementById("navsearch").addEventListener("mouseover",hidediv);
+
+
+
+let c=JSON.parse(localStorage.getItem("cart"))
+document.getElementById("navlen").innerText=c.length;
+if(c.length>0){
+    document.getElementById("navlen").style.backgroundColor="rgb(1,97,255)"
+}
+
+
+
+
+
